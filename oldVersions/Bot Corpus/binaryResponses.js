bc33edb492dd930052823fbb2c0c259da172baf9
epicTCK <caleblgentry@gmail.com>
2017-01-07 15:54:55 -0500

    Restructured Project

---
+++ oldVersions/Bot Corpus/binaryResponses.js
@@ -0,0 +1,49 @@
+
+var binary = 
+{
+"normal":[
+"Yes",
+"No",
+"no",
+"yes",
+"yeah",
+"nope",
+"never",
+"probly",
+"probobly",
+"Of Course",
+"Of course not",
+"of course",
+"of course not",
+"yup",
+"yep",
+"nien",
+"Affirmative.",
+"Negitive.",
+"I don't recall....",
+"I don't know",
+"IDK"
+],
+"Angry":
+[
+"Why would I ever",
+"why should i know?",
+"DO I LOOK LIKE I KNOW EVERYTHING",
+"GET OFF MY LAWN",
+"N.O.",
+"Y.E.S.",
+"YES",
+"NO",
+"yeaaahhhh.... no.",
+"what do you honestly think?"
+],
+"Funny":
+[
+"My answer is a superposition between 1 and 0. Because of course my source is running on quantum power.",
+"1",
+"0",
+"Never, Never, Never, Never",
+"Affirmative, Dave. I read you." ,
+"I'm sorry, Dave. I'm afraid I can't do that.",
+]
+}
